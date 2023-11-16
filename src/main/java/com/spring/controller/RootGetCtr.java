9214728bf746187702dd58eb7391c9a1e13ea46c
Acaibird <lian.wk04201@foxmail.com>
2023-11-16 20:11:11 +0800

    feat:使用RepenseEnty自定义响应返回体

--- src/main/java/com/spring/controller/RootGetCtr.java
+++ src/main/java/com/spring/controller/RootGetCtr.java
@@ -1,12 +1,21 @@
 package com.spring.controller;
 
+import com.spring.domain.A;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class RootGetCtr {
-    @GetMapping("/")
-    public String root() {
-        return "Hello World!";
+    @GetMapping("/json")
+    public ResponseEntity<A> root() {
+        A a = new A();
+        a.setStr("dasdasdf");
+        return new ResponseEntity<>(a, HttpStatus.OK);
     }
 }
+
