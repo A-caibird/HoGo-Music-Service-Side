a9dd7c804ab29412157af0fd5e7c988b8cec503b
Acaibird <lian.wk04201@foxmail.com>
2023-12-02 22:29:51 +0800

    feat: lombok日志文件配置,并在RootGet里面测试

--- src/main/java/com/spring/controller/RootGetCtr.java
+++ src/main/java/com/spring/controller/RootGetCtr.java
@@ -1,15 +1,18 @@
 package com.spring.controller;
 
 import com.spring.domain.RequestionParams.A;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j //lombok打印
 @RestController
 public class RootGetCtr {
     @GetMapping("/json")
     public ResponseEntity<A> root() {
+        log.info("info requestion RootGet");
         A a = new A();
         a.setStr("dasdasdf");
         return new ResponseEntity<>(a, HttpStatus.OK);
