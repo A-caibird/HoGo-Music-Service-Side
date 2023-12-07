b85dc90af3e17be32bcc281d5a43b0f64fb029f3
Acaibird <lian.wk04201@foxmail.com>
2023-12-07 09:00:17 +0800

    feat:添加事物异常处理 (unchecked)执行回滚

--- src/main/java/com/spring/controller/AddMusicCtr.java
+++ src/main/java/com/spring/controller/AddMusicCtr.java
@@ -5,12 +5,15 @@ import com.spring.domain.RequestionParams.AddMusicRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @CrossOrigin(origins = "*")
+@Transactional(rollbackFor = Exception.class)
 public class AddMusicCtr {
     private InsertMusic insertMusic;
 
@@ -18,6 +21,7 @@ public class AddMusicCtr {
     public void setInsertMusic(InsertMusic insertMusic) {
         this.insertMusic = insertMusic;
     }
+
     @PostMapping("/addMusic")
     public ResponseEntity<String> addMusic(@RequestBody AddMusicRequest rq) {
         int res = 0;
