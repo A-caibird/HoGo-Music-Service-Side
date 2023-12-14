7f5b05a4aa53e239d2182c650c3b13424308a0ee
Veledak <newcoder@icloud.com>
2023-12-14 13:31:26 +0800

    feat:修改上传的音乐文件名称,配置音乐文件的访问链接

--- src/main/java/com/spring/controller/UpMusicFileCtr.java
+++ src/main/java/com/spring/controller/UpMusicFileCtr.java
@@ -1,5 +1,8 @@
 package com.spring.controller;
 
+import com.spring.dao.MusicList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,11 +11,23 @@ import java.io.File;
 @RestController
 @CrossOrigin(origins = "*")
 public class UpMusicFileCtr {
+    @Value("${musicfileDir}")
+    private String musicfiledir;
+
+
+    private MusicList musicList;
+
+    @Autowired
+    public void setMusicList(MusicList musicList) {
+        this.musicList = musicList;
+    }
+
     @PostMapping("/uploadMusicFile")
     public String method(@RequestParam("mp3") MultipartFile file) throws Exception {
-        String fileName = file.getOriginalFilename();
-        System.out.println(fileName);
-        file.transferTo(new File("/Users/acaibird/Documents/git-project/cloud-music/public/mp3/"+fileName));
+        int cnt=musicList.getSongList().size();
+//      String fileName = file.getOriginalFilename();
+        String fileName = Integer.toString(cnt+1);
+        file.transferTo(new File(musicfiledir + fileName));
         return fileName;
     }
 }
