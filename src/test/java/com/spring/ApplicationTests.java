8a459366800467bd2dc76ae66dcb63d72c9517c6
Acaibird <newcoder@icloud.com>
2024-03-18 23:40:11 +0800

    fix:添加测试的配置类
    解决错误org.springframework.test.context.support.AnnotationConfigContextLoaderUtils -- Could not detect default configuration classes for test class [com.spring.ApplicationTests]: ApplicationTests does not declare any static, non-private, non-final, nested classes annotated with @Configuration.

--- src/test/java/com/spring/ApplicationTests.java
+++ src/test/java/com/spring/ApplicationTests.java
@@ -1,18 +1,14 @@
 package com.spring;
 
-import com.spring.service.authentication.email.EmailUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Config.class)
 class ApplicationTests {
-    @Autowired
-    private EmailUtil mailService;
 
     @Test
-    void contextLoads() {
-        mailService.sendSimpleMail("xxx@qq.com", "主题：你好普通邮件", "内容：第一封邮件");
+    void a() {
+      System.out.println(2);
     }
 
 }
